eb2de48030f480da7ce6d9640ff14cbe98129674
NGnYan <ngnyan@air-de-ngnyan.home>
2026-03-15 19:39:16 +0100

    chore: add publish.yml workflow

--- src/index.js
+++ src/index.js
@@ -1,1 +1,5 @@
 export { default as DataTable } from "./DataTable";
+export { default as DataTableHeader } from "./components/DataTableHeader";
+export { default as DataTableBody } from "./components/DataTableBody";
+export { default as DataTableRow } from "./components/DataTableRow";
+export { default as DataTableCell } from "./components/DataTableCell";
